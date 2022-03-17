ca762abc92716cff9e3471658170091da29ea7da
Andgel <a.ribeiro@pictia.io>
2022-03-17 16:12:40 +0100

    Fixes OpenClassrooms-Student-Center/Python_Testing#6

--- server.py
+++ server.py
@@ -46,7 +46,8 @@ def purchasePlaces():
     competition = [c for c in competitions if c['name'] == request.form['competition']][0]
     club = [c for c in clubs if c['name'] == request.form['club']][0]
     placesRequired = int(request.form['places'])
-    competition['numberOfPlaces'] = int(competition['numberOfPlaces'])-placesRequired
+    competition['numberOfPlaces'] = int(competition['numberOfPlaces']) - placesRequired
+    club['points'] = int(club['points']) - placesRequired
     flash('Great-booking complete!')
     return render_template('welcome.html', club=club, competitions=competitions)
 
